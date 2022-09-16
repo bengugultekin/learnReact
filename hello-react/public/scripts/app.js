edeefa817052aa5ccf8585ba3e0b702217b82934
bengugultekin <bengunurgultekin54@gmail.com>
2022-09-16 23:53:31 +0300

    using condition statements

--- hello-react/public/scripts/app.js
+++ hello-react/public/scripts/app.js
@@ -4,27 +4,31 @@ var root = document.getElementById('root');
 var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
 }, "Hello World!"), /*#__PURE__*/React.createElement("div", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum.")));
-var name = "Samsung S10";
-var price = 5000;
-var description = "Açıklama.....";
 var product = {
   name: "Samsung S10",
-  price: 5500,
-  description: "çok iyi bir telefon.."
+  price: 20000,
+  description: "çok iyi bir telefon..",
+  types: ['red', 'blue']
 };
 
 function formatPrice(p) {
   return p.price + 'TL';
 }
 
+function getDescription(description) {
+  if (description) {
+    return /*#__PURE__*/React.createElement("p", {
+      id: "product-desc"
+    }, "description: ", description);
+  } else {
+    return 'no desc';
+  }
+}
+
 var template2 = /*#__PURE__*/React.createElement("div", {
   id: "product-details"
 }, /*#__PURE__*/React.createElement("h2", {
   id: "product-name"
-}, "name: ", product.name), /*#__PURE__*/React.createElement("p", {
-  id: "product-price"
-}, "price: ", formatPrice(product)), /*#__PURE__*/React.createElement("p", {
-  id: "product-desc"
-}, "description: ", product.description)); // ReactDOM
+}, "name: ", product.name ? product.name : 'no name'), product.price && product.price > 0 && /*#__PURE__*/React.createElement("p", null, "price: ", formatPrice(product)), getDescription(product.description), /*#__PURE__*/React.createElement("p", null, product.types.length > 0 ? 'there are options' : 'no options')); // ReactDOM
 
 ReactDOM.render(template2, root);
--- hello-react/src/app.js
+++ hello-react/src/app.js
@@ -9,25 +9,33 @@ var template =  <div>
                         <li>Lorem, ipsum.</li>
                     </ul>
                 </div>;
-var name = "Samsung S10";
-var price = 5000;
-var description = "Açıklama.....";
 
 var product = {
     name: "Samsung S10",
-    price: 5500,
-    description: "çok iyi bir telefon.."
+    price: 20000,
+    description: "çok iyi bir telefon..",
+    types: ['red', 'blue']
 }
 
 function formatPrice(p) {
     return p.price + 'TL';
 }
 
+function getDescription(description) {
+    if(description) {
+        return <p id="product-desc">description: {description}</p>;
+    }
+    else {
+        return 'no desc';
+    }
+}
+
 var template2 = (
     <div id="product-details">
-        <h2 id="product-name">name: {product.name}</h2>
-        <p id="product-price">price: {formatPrice(product)}</p>
-        <p id="product-desc">description: {product.description}</p>
+        <h2 id="product-name">name: {product.name ? product.name : 'no name'}</h2>
+        {(product.price && product.price > 0) && <p>price: {formatPrice(product)}</p>}
+        {getDescription(product.description)}
+        <p>{product.types.length > 0 ?'there are options' : 'no options'}</p>
     </div>
 );
 // ReactDOM
