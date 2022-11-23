e2a2f86c580f61738c7fca5683e9bc59d0faedf8
bengugultekin <bengunurgultekin54@gmail.com>
2022-11-23 23:10:38 +0300

    updated users information

--- githubFinder/src/components/Users.js
+++ githubFinder/src/components/Users.js
@@ -32,33 +32,25 @@ class Users extends Component {
                     name: "Sadık Turan",
                     blog: "sadikturan.com",
                     followers: 3984
-                  },{
-                    login: "onurgurkangultekin",
-                    id: 8975671,
-                    avatar_url: "https://avatars.githubusercontent.com/u/8975671?v=4",
-                    html_url: "https://github.com/onurgurkangultekin",
-                    name: "Onur Gürkan GÜLTEKİN",
-                    blog: "http://onurgultekin.blogspot.com.tr/",
-                    followers: 10,
                   },
                   {
-                    login: "bengugultekin",
-                    id: 44199167,
-                    avatar_url: "https://avatars.githubusercontent.com/u/44199167?v=4",
-                    html_url: "https://github.com/bengugultekin",
-                    name: "Bengünur Gültekin",
-                    blog: "www.bengugultekin.blogspot.com",
-                    followers: 9
+                    login: "mojombo",
+                    id: 1,
+                    avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+                    html_url: "https://github.com/mojombo",
                   },
                   {
-                    login: "sadikturan",
-                    id: 19492591,
-                    avatar_url: "https://avatars.githubusercontent.com/u/19492591?v=4",
-                    html_url: "https://github.com/sadikturan",
-                    name: "Sadık Turan",
-                    blog: "sadikturan.com",
-                    followers: 3984
-                  }
+                    login: "defunkt",
+                    id: 2,
+                    avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
+                    html_url: "https://github.com/defunkt",
+                  },
+                  {
+                    login: "pjhyett",
+                    id: "3",
+                    avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
+                    html_url: "https://github.com/pjhyett",
+                  },
                   
                   
             ]
