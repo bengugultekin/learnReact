f7b700b6b0a80445204b94d39ef9430cc3c38ae9
bengugultekin <bengunurgultekin54@gmail.com>
2022-11-24 15:18:38 +0300

    learned route links

--- githubFinder/src/index.js
+++ githubFinder/src/index.js
@@ -2,31 +2,61 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './styles/main.scss';
 import App from './components/App';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+
+const Nav = () => (
+    // <nav>
+    //     <Link to="/">Home</Link> |
+    //     <Link to="/contact">Contact</Link> |
+    //     <Link to="/products">Products</Link>
+    // </nav>
+
+    <nav>
+        <NavLink exact to="/" activeClassName="active" >Home</NavLink> |
+        <NavLink to="/contact" activeClassName="active">Contact</NavLink> |
+        <NavLink to="/products" activeClassName="active">Products</NavLink>
+    </nav>
+);
+
+const Header = () => (
+    <header>
+        <h1>Github Finder</h1>
+    </header>
+);
 
 const HomePage = () => (
-    <div>Home Page</div>
+    <>
+        <div>Home Page</div>
+    </>
 );
 
 const ContactPage = () => (
-    <div>Contact Page</div>
+    <>
+        <div>Contact Page</div>
+    </>
 );
 
 const ProductsPage = () => (
-    <div>Products Page</div>
+    <>
+        <div>Products Page</div>
+    </>
 );
 
-const NotFound = () => (
-    <div>Your page not found...</div>
+const NotFoundPage = () => (
+    <>
+        <div>You page not found...</div>
+    </>
 );
 
 const routes = (
     <BrowserRouter>
+        <Header />
+        <Nav />
         <Switch>
             <Route exact path="/" component={HomePage} />
             <Route path="/contact" component={ContactPage} />
             <Route path="/products" component={ProductsPage} />
-            <Route component={NotFound}/>
+            <Route component={NotFoundPage}/>
         </Switch>
     </BrowserRouter>
 );
