fe7a139fe24971509d263eb1ef291e3e10184a27
bengugultekin <bengunurgultekin54@gmail.com>
2022-11-24 10:19:33 +0300

    searched word in searchbar filtered from api

--- githubFinder/src/components/App.js
+++ githubFinder/src/components/App.js
@@ -7,6 +7,7 @@ import Search from './Search'
 export class App extends Component {
     constructor(props) {
         super(props);
+        this.searchUsers = this.searchUsers.bind(this);
         this.state = {
             loading: false,
             users: []
@@ -21,13 +22,21 @@ export class App extends Component {
         }, 1000);
         
     }
+    searchUsers(keyword) {
+        this.setState({loading: true});
+        setTimeout(() => {
+            Axios
+        .get(`https://api.github.com/search/users?q=${keyword}`)
+        .then(response => this.setState({users: response.data.items, loading:false}));
+        }, 1000);
+        
+    }
   render() {
     return (
         <Fragment>
             <Navbar />
-            <Search />
+            <Search searchUsers={this.searchUsers}/>
             <Users users = {this.state.users} loading={this.state.loading}/>
-
         </Fragment>
     )
   }
