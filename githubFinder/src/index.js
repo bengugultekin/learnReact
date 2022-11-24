1adb3fa5b91c2596ef60f133919dec7396a25af7
bengugultekin <bengunurgultekin54@gmail.com>
2022-11-24 15:42:24 +0300

    learned query strings & url parameters

--- githubFinder/src/index.js
+++ githubFinder/src/index.js
@@ -1,64 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './styles/main.scss';
+import AppRouter from "./routers/AppRouter";
 import App from './components/App';
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 
-const Nav = () => (
-    // <nav>
-    //     <Link to="/">Home</Link> |
-    //     <Link to="/contact">Contact</Link> |
-    //     <Link to="/products">Products</Link>
-    // </nav>
-
-    <nav>
-        <NavLink exact to="/" activeClassName="active" >Home</NavLink> |
-        <NavLink to="/contact" activeClassName="active">Contact</NavLink> |
-        <NavLink to="/products" activeClassName="active">Products</NavLink>
-    </nav>
-);
-
-const Header = () => (
-    <header>
-        <h1>Github Finder</h1>
-    </header>
-);
-
-const HomePage = () => (
-    <>
-        <div>Home Page</div>
-    </>
-);
-
-const ContactPage = () => (
-    <>
-        <div>Contact Page</div>
-    </>
-);
-
-const ProductsPage = () => (
-    <>
-        <div>Products Page</div>
-    </>
-);
-
-const NotFoundPage = () => (
-    <>
-        <div>You page not found...</div>
-    </>
-);
-
-const routes = (
-    <BrowserRouter>
-        <Header />
-        <Nav />
-        <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/contact" component={ContactPage} />
-            <Route path="/products" component={ProductsPage} />
-            <Route component={NotFoundPage}/>
-        </Switch>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('root'));
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
